8bf9acc042dd408fe271f4fdc085258c70112720
suramya-biswas-git <suramya.biswas@gmail.com>
2024-06-24 17:08:01 +0530

    2nd Version

---
+++ myOwnApp/src/main/java/com/suramya/myOwnApp/MyDemo.java
@@ -0,0 +1,8 @@
+package com.suramya.myOwnApp;
+
+public class MyDemo {
+	public void show() {
+		System.out.println(" hello hi");
+	}
+
+}
--- myOwnApp/src/main/java/com/suramya/myOwnApp/MyOwnAppApplication.java
+++ myOwnApp/src/main/java/com/suramya/myOwnApp/MyOwnAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyOwnAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(MyOwnAppApplication.class, args);
 	}
 
